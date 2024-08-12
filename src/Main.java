ee0c9d2baec7af78c19ea62b592f087cefc75d16
Antonio Lunguinho <antoniolunguinho007@gmail.com>
2024-08-12 11:58:37 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Calendar cal = GregorianCalendar.getInstance();
+        Scanner scan = new Scanner(System.in);
+
+        int anoAtual = cal.get(Calendar.YEAR);
+        int anoNascimento;
+        int idadeAtual;
+        String situacaoVoto;
+        String completouAno;
+
+        String anoEleitoral = (anoAtual % 2 == 0) ?
+                "Esse ano terá eleições!" : "Esse ano não terá eleições!";
+
+        System.out.print("Você já completou ano nesse ano? [S/N] ");
+        completouAno = scan.nextLine();
+
+        System.out.print("Digite o ano de nascimento: ");
+        anoNascimento = scan.nextInt();
+
+        idadeAtual = anoAtual - anoNascimento;
+
+        if (Objects.equals(completouAno, "N")) {
+            --idadeAtual;
+        }
+
+        if (idadeAtual <= 15){
+            situacaoVoto = "Você não vota!";
+        } else if ((idadeAtual >= 16 && idadeAtual < 18) || (idadeAtual >= 70)) {
+            situacaoVoto = "Seu voto é opcional!";
+        } else {
+            situacaoVoto = "Seu voto é obrigatório!";
+        }
+
+        System.out.println("----------------------------------");
+        System.out.println(anoEleitoral);
+        System.out.println("Você tem " + idadeAtual + " anos de idade, portanto");
+        System.out.println(situacaoVoto);
+
+//        System.out.println("Você tem " + idadeAtual + " anos de idade!");
+//        if (idadeAtual <= 15) {
+//            System.out.println("Seu voto é proibido!");
+//        } else {
+//            situacaoVoto = ((idadeAtual >= 16 && idadeAtual < 18) || (idadeAtual > 70)) ?
+//                    "Seu voto é opcional!" : "Seu voto é obrigatório!";
+//            System.out.println(situacaoVoto);
+//        }
+
+    }
+}
